561785d420ba48ef86e6eef96b65209dd38f4232
서성진 <tjtjdwls0614@gmail.com>
2025-03-06 14:38:14 +0900

    폰트, 이미지, 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/Notfound'
 
-
+import { getEmotionImage } from './util/get-emotion-image'
 
 function App() {
   const nav = useNavigate()
@@ -16,6 +16,13 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
